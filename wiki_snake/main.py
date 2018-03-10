d7fc493c34979372f3a1d7e7986107c03fa10663
Dario Giovannetti <dev@dariogiovannetti.net>
2018-03-10 19:11:56 +0800

    Continue initial implementation

--- wiki_snake/main.py
+++ wiki_snake/main.py
@@ -16,17 +16,17 @@
 # You should have received a copy of the GNU General Public License
 # along with Wiki Monkey.  If not, see <http://www.gnu.org/licenses/>.
 
-# NOTE: Due to werkzeug issue #461 this is the only configuration to get this
+# NOTE: WHY NOT MOVE 'api' AND 'models' NEXT TO THIS SCRIPT?
+# Due to werkzeug issue #461 this is the only configuration to get this
 # app running with Flask in debug=True mode (in debug=False mode everything
 # would work normally); the problem is that in debug mode the reloader is
 # activated, and it messes the PYTHONPATH at every app reload, thus failing
-# the relative module imports. It is necessary to keep the modules in a 'lib'
-# subpackage (they can't stay as siblings of this script); this script has to
-# be called with 'python -m main' from within its directory (trying
-# 'python -m server.main' from the parent directory fails).
+# the relative module imports. It is necessary to keep the subpackages inside a
+# common'lib' subpackage (they can't stay as siblings of this script); this
+# script has to be called with 'python -m main' from within its directory
+# (trying 'python -m server.main' from the parent directory fails).
 # https://github.com/pallets/werkzeug/issues/461
 # https://chase-seibert.github.io/blog/2015/06/12/flask-werkzeug-reloader-python-dash-m.html
 
 if __name__ == "__main__":
-    from lib import app
-    app.run()
+    from lib import app  # noqa
