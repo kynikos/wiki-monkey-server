b19ad0989c8a57d3b3e761b121b78210f6c35853
Dario Giovannetti <dev@dariogiovannetti.net>
2018-04-25 02:56:54 +0800

    Continue initial implementation

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wiki-snake',
+    version='0.1.0',
+    description=("Wiki Monkey's server."),
+    long_description=("Wiki Monkey's server."),
+    url='https://github.com/kynikos/wiki-snake',
+    author='Dario Giovannetti',
+    author_email='dev@dariogiovannetti.net',
+    license='GPLv3+',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  # noqa
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Database :: Database Engines/Servers',
+    ],
+    keywords='flask rest restful api marshal marshmallow',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+)
