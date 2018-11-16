1a37d6ec9fa97df359a0942d2a7c143c346e0366
Dario Giovannetti <dev@dariogiovannetti.net>
2018-11-16 22:55:58 +0800

    Improve setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='wiki-snake',
     version='0.1.0',
-    description=("Wiki Monkey's database server."),
-    long_description=("Wiki Monkey's database server."),
+    description="Wiki Monkey's database server.",
+    long_description="Wiki Monkey's database server.",
     url='https://github.com/kynikos/wiki-snake',
     author='Dario Giovannetti',
     author_email='dev@dariogiovannetti.net',
     license='GPLv3+',
-    classifiers=[
+    classifiers=(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Flask',
@@ -19,22 +19,22 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Database :: Database Engines/Servers',
-    ],
+    ),
     keywords='wikimonkey wiki-monkey wiki mediawiki archwiki wikipedia',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=('contrib', 'docs', 'tests')),
     entry_points={
-        'console_scripts': [
+        'console_scripts': (
             # Use wiki-monkey, not wiki-snake, since that's the application
             # that users want to use after all
             # TODO: Maybe just rename the whole project as wiki-monkey-server
             'wiki-monkey = wiki_snake.main',
             'wiki-monkey-gencert = wiki_snake.gencert',
-        ],
+        ),
     },
     data_files=[
         # Relative paths are put inside the installation folder
-        ('srv/static/', ['wiki-monkey/dist/*.js']),
+        ('srv/static/', ('wiki-monkey/dist/*.js')),
         # "-" has a special meaning in systemd unit names
-        ('/usr/lib/systemd/user/', ['auxiliary/wiki_monkey.service']),
+        ('/usr/lib/systemd/user/', ('auxiliary/wiki_monkey.service')),
     ],
 )
