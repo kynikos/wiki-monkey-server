7a4cf627e9d6698688c2a2e59f9f21db4a3e7b0b
Dario Giovannetti <dev@dariogiovannetti.net>
2020-08-10 01:07:01 +0800

    v5.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wiki-monkey',
-    version='5.1.3',
+    version='5.2.0',
     description="Wiki Monkey - MediaWiki bot and editor-assistant user script.",
     long_description="Wiki Monkey - MediaWiki (ArchWiki-optimized) bot and "
         "editor-assistant user script (server-enabled version).",
